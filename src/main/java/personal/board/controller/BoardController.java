232ffbbcacebbd89ae0d8445734fd0b1a914ce95
howudong <tjdvy963@naver.com>
2024-03-01 00:04:39 +0900

    feat : 게시물 상세 조회 및 게시판 조회 기능 구현, 관련된 페이지 구현

--- src/main/java/personal/board/controller/BoardController.java
+++ src/main/java/personal/board/controller/BoardController.java
@@ -4,12 +4,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import personal.board.domain.entity.Board;
 import personal.board.repository.BoardRepository;
+
+import java.util.List;
+import java.util.NoSuchElementException;
 
 @Controller
 @Slf4j
@@ -19,7 +19,7 @@ public class BoardController {
     private final BoardRepository boardRepository;
 
     @GetMapping("/add")
-    public String board(Model model) {
+    public String addBoardForm(Model model) {
         model.addAttribute("board", new Board());
         return "addBoard";
     }
@@ -28,5 +28,23 @@ public class BoardController {
     public String add(@ModelAttribute Board board) {
         boardRepository.save(board);
         return "redirect:/";
+    }
+
+    @RequestMapping()
+    public String boards(Model model) {
+        List<Board> boards = boardRepository.findAll();
+        model.addAttribute("boards", boards);
+        return "boards";
+    }
+
+    @GetMapping("/{id}")
+    public String board(Model model, @PathVariable("id") Long id) {
+        Board board = boardRepository.findById(id)
+                .orElseThrow(
+                        () -> new NoSuchElementException("없음"));
+
+        model.addAttribute("board", board);
+
+        return "board";
     }
 }
